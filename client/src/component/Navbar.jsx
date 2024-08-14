9586ebda7ec9331eaa9dbef1502053f8dfa2f743
RajithaUBandara21 <138967884+RajithaUBandara21@users.noreply.github.com>
2024-08-14 09:23:58 +0530

    add logos and staart nav bar developing

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,10 +1,14 @@
+import {HiMenuAlt4} from 'react-icons/hi';
+import {AiOutlineClose} from 'react-icons/ai';
 
+
+import logo from '../../images/logo.png';
 
 
 const Navbar = () => {
     return (
-        <nav>
-            <h1>Navbar</h1>
+        <nav className="w-full flex md:justify-center justify-between items-center p-4 ">
+           <div className="md:flex-[0.5] flex-initial justify-">
         </nav>
     )
 }
